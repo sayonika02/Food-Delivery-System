7f69fe093b091e84d5ad7a68def3d59f2b4c4a97
Sayonika Das <64629985+sayonika02@users.noreply.github.com>
2021-11-23 09:28:44 +0530

    Add files via upload

---
+++ food_del/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+def main():
+    
+    menu = ["Login", "Signup"]
+    choice = st.sidebar.selectbox("Menu", menu)
+
+    if choice == "Login":
+        st.title('Login')
+        st.subheader("Welcome Back!")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type = 'password')
+        if st.button("Login"):
+            st.success("Logged In as {}". format(username))
+    elif choice == "Signup":
+        st.title('Signup')
+        st.subheader("Create New Account")
+
+if __name__ == '__main__':
+    main()
