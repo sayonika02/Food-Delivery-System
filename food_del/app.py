76a6cc278fce9722de84aefaabc82a386bc68d0b
Sayonika Das <64629985+sayonika02@users.noreply.github.com>
2021-11-23 09:48:24 +0530

    Update app.py

--- food_del/app.py
+++ food_del/app.py
@@ -19,3 +19,25 @@ def main():
 
 if __name__ == '__main__':
     main()
+    
+# Initialize connection.
+# Uses st.cache to only run once.
+@st.cache(allow_output_mutation=True, hash_funcs={"_thread.RLock": lambda _: None})
+def init_connection():
+    return psycopg2.connect(**st.secrets["postgres"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache to only rerun when the query changes or after 10 min.
+@st.cache(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from site;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]}")
